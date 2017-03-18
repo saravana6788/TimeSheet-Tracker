fa2590dcf773e8b873155bbe1cea888265771b7b
SARAVANA KUMAR GANAPATHY <saravanan.ganapathy@gmail.com>
2017-03-17 17:36:32 -0700

    Add files via upload

---
+++ timetracker.sql
@@ -0,0 +1,11 @@
+
+create schema if not exists track;
+
+CREATE TABLE track.timesheet_status (
+  `S_NO` int(11) NOT NULL,
+  `EMP_ID` int(11) DEFAULT NULL,
+  `EMP_NAME` varchar(50) DEFAULT NULL,
+  `TIMESHEET` varchar(50) DEFAULT NULL,
+  `SUBMISSION_STATUS` char(2) DEFAULT NULL,
+  PRIMARY KEY (`S_NO`);
+
